830b033caa8203725f91b3597ab1c88c7af994cd
Mizz1ove <pa990512@naver.com>
2023-12-15 10:37:16 +0900

    Feat : port

--- src/main/java/com/nhnacademy/aiot/Port.java
+++ src/main/java/com/nhnacademy/aiot/Port.java
@@ -1,15 +1,25 @@
 package com.nhnacademy.aiot;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Port {
 
     private LinkedList<Wire> wireList;
 
+    public Port(){
+        this.wireList = new LinkedList<>();
+    }
 
     public void out(Message message) {
+        if(message != null){
+            for(Wire wire : wireList){
+                wire.push(message);
+            }
+        }
     }
 
     public void bindWire(Wire wire) {
+        wireList.add(wire);    
     }
 }
--- src/main/java/com/nhnacademy/aiot/Wire.java
+++ src/main/java/com/nhnacademy/aiot/Wire.java
@@ -1,4 +1,21 @@
 package com.nhnacademy.aiot;
 
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Wire {
+    Queue<Message> messageQueue;
+    
+    public Wire(){
+        messageQueue = new LinkedBlockingDeque<>();
+    }
+
+    public Message get() {
+        return messageQueue.poll();
+    }
+
+    public void push(Message msg) {
+        messageQueue.add(msg);
+    }
+
 }
