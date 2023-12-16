a93f88236eac65a716900ffa296d36bdeb13580d
sylim <sammys27@naver.com>
2023-12-16 13:49:01 +0900

    add Msg, Port, Wire

--- src/main/java/com/nhnacademy/aiot/Message.java
+++ src/main/java/com/nhnacademy/aiot/Message.java
@@ -7,6 +7,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class Message {
     String id;
     ObjectNode objectNode;
@@ -14,10 +21,34 @@ public class Message {
 
     public Message(String topic, MqttMessage message){
 
+        String payload =  new String(message.getPayload());
+
     }
 
     public Message(ObjectNode objectNode){
         this.objectNode = objectNode;
         this.id = UUID.randomUUID().toString();
     }
+
+
+    // Todo : 원하는 value 를 String 으로 return
+    public JsonNode PhasePath(String paths, String payload) {
+
+        String str = payload;
+        JsonNode jsonNode = JSONUtils.parseJson(str);
+
+        String[] arr = getPhasePath(paths);
+        for (int i = 0; i < arr.length; i++) {
+            jsonNode = jsonNode.path(arr[i]);
+        }
+        return jsonNode;
+
+    }
+
+    // Todo : "."을 포함한 String으로 받아서 배열로 return
+    public String[] getPhasePath(String paths) {
+        String[] pathArr = paths.split("\\.");
+        return pathArr;
+    }
+
 }
--- src/main/java/com/nhnacademy/aiot/Port.java
+++ src/main/java/com/nhnacademy/aiot/Port.java
@@ -6,10 +6,35 @@ public class Port {
 
     private LinkedList<Wire> wireList;
 
+    public Port(){
+        this.wireList = new LinkedList<>();
+    }
 
     public void out(Message message) {
+        if (message != null) {
+            for (Wire wire : wireList) {
+                wire.put(message);
+            }
+        }
+    }
+
+    public Message get() {
+
+        for (Wire wire : wireList) {
+            if (wire.hasMessage()) {
+                return wire.messageQueue.poll();
+            }
+        }
+        return null;
     }
 
     public void bindWire(Wire wire) {
+        wireList.add(wire);
     }
+
+    public LinkedList<Wire> getWires() {
+        return wireList;
+    }
+
+}
 }
